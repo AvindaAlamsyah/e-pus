40db08d8acccc2c4f5a2d6b7ddddf031dbe4480d
Avinda Alamsyah <avindaalamsyah@gmail.com>
2021-05-11 13:35:56 +0700

    fix tampilan list buku

--- application/models/Model_peminjaman.php
+++ application/models/Model_peminjaman.php
@@ -6,12 +6,24 @@ class Model_peminjaman extends CI_Model {
 
     private $table = "peminjaman";
 
-    function select_where($where)
+    function select_join_where($where)
     {
         $this->db->select('buku.judul_buku, peminjaman.id_peminjaman');
         $this->db->join('buku', 'buku.id_buku = peminjaman.buku_id_buku', 'inner');
         
         return $this->db->get_where($this->table, $where);
+        
+    }
+
+    function select_where($where)
+    {
+        return $this->db->get_where($this->table, $where);
+        
+    }
+
+    function select_all()
+    {
+        return $this->db->get($this->table);
         
     }
 
