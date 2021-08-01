ba2786b083d6c7c3394ab0d1ebdcea2ffda5a169
raismaulana <lufriraismaulana@gmail.com>
2021-08-01 17:34:02 +0700

    setup phpdotenv

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/e-pus/';
+$config['base_url'] = $_ENV['CONFIG_BASE_URL'];
 
 /*
 |--------------------------------------------------------------------------
@@ -380,7 +380,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = (empty($_ENV['CONFIG_SESS_SAVE_PATH'])) ? null : APPPATH . $_ENV['CONFIG_SESS_SAVE_PATH'];
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
